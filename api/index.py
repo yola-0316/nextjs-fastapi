b8c0fbc0c868dfabeb82da46d840f6751bbd74bb
Zhi.Huang <zh.impact@outlook.com>
2023-05-31 03:20:51 +0800

    update: detect audio file info

--- api/index.py
+++ api/index.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+# import whisper
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/audio")
+def whisper():
+    return {"message": "Please using button to upload an audio file."}
+
+@app.post("/api/audio")
+def whisper_extract(audio: UploadFile):
+    print(audio.filename, audio.content_type)
+    # model = whisper.load_model("base")
+    # result = model.transcribe("audio.mp3")
+    # print(result["text"])
+    return {"message": "Upload an audio file.", "filename": audio.filename, "content_type": audio.content_type}
